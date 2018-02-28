922204358b6c2f75519bc937c763782492c934b2
aa11ac <ach.aayush.111@gmail.com>
2018-02-28 22:05:43 +0545

    Add existing file

--- jason.js
+++ jason.js
@@ -1,7 +1,17 @@
-function api(){$.getJASON('https://talaikis.com/api/quotes/random/')
-.done(function(data){
-    $(".text").text(data.quote);
-})}
-api();
-
-$("#newQuote").click(alert("hello"));
+var i;
+colors=["red","yellow","green","blue"]
+function getQuote(){
+    $.get("https://talaikis.com/api/quotes/random/")
+    .done(function(data){
+        $("#quote").text(data.quote);
+        $("#author").text(`- ${data.author}`);
+    })
+    .fail(function(){
+        console.log("Opps some error occured")
+      $("body").css("backgroundColor",colors[i]);
+    })
+  i+=1;
+  if (i<=3){i=0;}
+}
+getQuote();
+$(".btn").click(getQuote);
