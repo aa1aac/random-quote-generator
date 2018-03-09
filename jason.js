f11fd0718d3fe6d7e868fb913218598525b0bf83
aa11ac <ach.aayush.111@gmail.com>
2018-03-09 22:47:15 +0545

    update

--- jason.js
+++ jason.js
@@ -1,10 +1,24 @@
-var i;
-colors=["red","yellow","green","blue"]
+var i=0;
+colors=["rgb(0, 0, 0)",
+"rgb(241, 62, 62)",
+"rgb(211, 255, 14)",
+"rgb(255, 0, 128)",
+"rgb(0, 26, 255)",
+]
+var body=document.querySelector("body");
+function changeColor(){
+ body.style.backgroundColor=colors[i];
+ if (i==4){
+   i=0;
+ }
+ i=i+1;
+}
 function getQuote(){
     $.get("https://talaikis.com/api/quotes/random/")
     .done(function(data){
         $("#quote").text(data.quote);
         $("#author").text(`- ${data.author}`);
+        changeColor();
     })
     .fail(function(){
         console.log("Opps some error occured")
